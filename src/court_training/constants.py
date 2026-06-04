b9715087d7db72d1021c23aeea1be004d8b9df6e
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-06-04 22:01:24 +0200

    Add DINO segmenter inference

--- src/court_training/constants.py
+++ src/court_training/constants.py
@@ -1,5 +1,6 @@
 from torch import tensor
 
+BACKBONE = "vit_small_plus_patch16_dinov3"
 MASK_NAMES = (
     "left_court",
     "right_court",
@@ -9,5 +10,6 @@ MASK_NAMES = (
     "right_paint",
 )
 LEFT_RIGHT_PAIRS = ((0, 1), (2, 3), (4, 5))
+TTA_SCALES = (0.75, 0.9, 1.0, 1.1, 1.25)
 IMAGE_MEAN = tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
 IMAGE_STD = tensor([0.229, 0.224, 0.225]).view(3, 1, 1)

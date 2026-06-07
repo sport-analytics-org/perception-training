1217d1ada7ccf9eb34bc2fb9aa140f22cf615c10
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-06-07 10:51:51 +0200

    Add jaxtyping dataset sample hints

--- src/court_training/data.py
+++ src/court_training/data.py
@@ -3,6 +3,7 @@ from typing import TypedDict
 
 import numpy as np
 import torch
+from jaxtyping import Float
 from PIL import Image
 from torch.utils.data import Dataset
 
@@ -10,8 +11,8 @@ from court_training.constants import IMAGE_MEAN, IMAGE_STD, MASK_NAMES
 
 
 class MaskSample(TypedDict):
-    image: torch.Tensor
-    mask: torch.Tensor
+    image: Float[torch.Tensor, "channel height width"]  # noqa: F722
+    mask: Float[torch.Tensor, "mask height width"]  # noqa: F722
 
 
 class MaskDataset(Dataset):
@@ -42,12 +43,12 @@ def image_mask_pairs(root: Path, dataset_names: tuple[str, ...]) -> list[tuple[P
     return pairs
 
 
-def image_to_tensor(image: Image.Image) -> torch.Tensor:
+def image_to_tensor(image: Image.Image) -> Float[torch.Tensor, "channel height width"]:  # noqa: F722
     array = np.asarray(image, dtype=np.float32) / 255.0
     image_tensor = torch.from_numpy(array).permute(2, 0, 1)
     return (image_tensor - IMAGE_MEAN) / IMAGE_STD
 
 
-def bitfield_to_masks(bitfield: np.ndarray) -> torch.Tensor:
+def bitfield_to_masks(bitfield: np.ndarray) -> Float[torch.Tensor, "mask height width"]:  # noqa: F722
     masks = [(bitfield & (1 << bit)) > 0 for bit in range(len(MASK_NAMES))]
     return torch.from_numpy(np.stack(masks).astype(np.float32))
